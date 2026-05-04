7e7d075569a59842006fe619bc24af28b5ab2bea
vijaytemp104-crypto <vijaytemp104@gmail.com>
2026-05-04 22:33:56 +0530

    Delete src directory

--- src/InventoryManagement/Alertable.java
+++
@@ -1,6 +0,0 @@
-package InventoryManagement;
-public interface Alertable {
-    int LOW_STOCK_THRESHOLD = 5;
-    boolean isLowStock();
-    void triggerLowStockAlert();
-}
--- src/InventoryManagement/InventoryEntity.java
+++
@@ -1,4 +0,0 @@
-package InventoryManagement;
-public abstract class InventoryEntity {
-    public abstract void display();
-}
